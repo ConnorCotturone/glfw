9928eb27eb5556c2da11f26069dea4921b413893
Connor Cotturone <ccotturo@vols.utk.edu>
2024-01-16 14:43:24 -0500

    premake update for working window

--- premake5.lua
+++ premake5.lua
@@ -7,14 +7,19 @@ project "GLFW"
 
 	files {
         "include/GLFW/glfw3.h",
-        "include/GLFW/glfw3native.h",
-        "src/glfw_config.h",
-        "src/context.c",
-        "src/init.c",
-        "src/input.c",
-        "src/monitor.c",
-        "src/vulkan.c",
-        "src/window.c"
+		"include/GLFW/glfw3native.h",
+		"src/glfw_config.h",
+		"src/context.c",
+		"src/input.c",
+		"src/monitor.c",
+		"src/init.c",
+		"src/vulkan.c",
+		"src/window.c",
+		"src/platform.c",
+		"src/null_init.c",
+		"src/null_monitor.c",
+		"src/null_window.c",
+		"src/null_joystick.c"
     }
 
 	filter "system:windows"
@@ -23,14 +28,15 @@ project "GLFW"
 
         files {
             "src/win32_init.c",
-            "src/win32_joystick.c",
-            "src/win32_monitor.c",
-            "src/win32_time.c",
-            "src/win32_thread.c",
-            "src/win32_window.c",
-            "src/wgl_context.c",
-            "src/egl_context.c",
-            "src/osmesa_context.c"
+			"src/win32_joystick.c",
+			"src/win32_monitor.c",
+			"src/win32_time.c",
+			"src/win32_thread.c",
+			"src/win32_window.c",
+			"src/wgl_context.c",
+			"src/egl_context.c",
+			"src/osmesa_context.c",
+			"src/win32_module.c"
         }
 
 		defines { 
